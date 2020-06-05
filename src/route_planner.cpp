59e4362ed21e6a9c655f5d5bef5f615e1cf47e80
Marcos Santana <brmarcossantanabr@gmail.com>
2020-06-04 21:27:21 -0300

    Find the next node

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -54,7 +54,14 @@ void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
 // - Return the pointer.
 
 RouteModel::Node *RoutePlanner::NextNode() {
-
+    std::sort(open_list.begin(), open_list.end(), [](const auto & _1st, const auto & _2nd) {
+		return (_1st->h_value + _1st->g_value) < (_2nd->h_value + _2nd->g_value);
+	});
+  
+    RouteModel::Node* lowest = open_list.front();
+	open_list.erase(open_list.begin());
+  
+    return lowest;
 }
 
 
